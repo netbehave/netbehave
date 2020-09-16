8db619769a61dd9b31a867357c098b005d61788d
Yves Desharnais (NetBehave) <info@netbehave.com>
2020-09-16 13:58:03 -0400

    Added public and v2_log fields

--- v2/netbehave-pg-initdbv2/rootfs/code/v2-base.sql
+++ v2/netbehave-pg-initdbv2/rootfs/code/v2-base.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS timestamp_object (
+CREATE TABLE IF NOT EXISTS public.timestamp_object (
   date_added    TIMESTAMP DEFAULT NOW(),
   last_seen 	TIMESTAMP DEFAULT NOW(),
   last_modified	TIMESTAMP DEFAULT NOW()
@@ -20,15 +20,17 @@ DROP TRIGGER IF EXISTS update_timestamp_object_changetimestamp
 	ON public.timestamp_object;
 
 CREATE TRIGGER update_timestamp_object_changetimestamp BEFORE UPDATE
-    ON timestamp_object FOR EACH ROW EXECUTE PROCEDURE 
+    ON public.timestamp_object FOR EACH ROW EXECUTE PROCEDURE 
     update_changelast_modified_column();
 
 
 
 -- TODO: trigger for UPDATE to save old/new values
-CREATE TABLE IF NOT EXISTS v2_log (
+CREATE TABLE IF NOT EXISTS public.v2_log (
   table_name TEXT,
   table_field TEXT,
+  table_key TEXT,
+  table_key_value TEXT,
   old_value TEXT,
   new_value TEXT,
   date_added    TIMESTAMP DEFAULT NOW()
